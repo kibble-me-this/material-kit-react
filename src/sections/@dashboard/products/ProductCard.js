9ba09e837d8f91bd9eaae82d1a8112e6861e3750
Carlos Herrera <carlos@petastic.com>
2023-04-16 17:39:56 -0400

    restyled cards

--- src/sections/@dashboard/products/ProductCard.js
+++ src/sections/@dashboard/products/ProductCard.js
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 // @mui
 import { styled } from '@mui/material/styles';
 import { Box, Card, Link, Typography, Stack, Divider } from '@mui/material';
@@ -14,6 +15,14 @@ import { ColorPreview } from '../../../components/color-utils';
 
 const StyledCard = styled(Card)(({ theme }) => ({
   ...bgBlur({ color: theme.palette.background.pink }),
+  position: 'relative',
+  width: '100%',
+  height: '100%',
+  transformStyle: 'preserve-3d',
+  transition: 'transform 0.6s',
+  '&.flipped': {
+    transform: 'rotateY(180deg)',
+  },
 }));
 
 const StyledProductImg = styled('img')({
@@ -24,28 +33,54 @@ const StyledProductImg = styled('img')({
   position: 'absolute',
 });
 
+const styles = {
+  flipCard: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    backfaceVisibility: 'hidden',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+    padding: 16,
+  },
+  flipCardFront: {
+    backgroundColor: '#bbb',
+    color: 'black',
+    zIndex: 2,
+  },
+  flipCardBack: {
+    backgroundColor: '#2980b9',
+    color: 'white',
+    transform: 'rotateY(180deg)',
+    zIndex: 1,
+  },
+};
+
 // ----------------------------------------------------------------------
 
 ShopProductCard.propTypes = {
   product: PropTypes.object,
 };
 
+function FrontCard() {
+  return (
+    <Box className={styles.flipCardFront}>
+      <Typography variant="subtitle2">Front Card</Typography>
+    </Box>
+  );
+}
 
+function BackCard() {
+  return (
+    <Box className={styles.flipCardBack}>
+      <Typography variant="subtitle2">Back Card</Typography>
+    </Box>
+  );
+}
 
 export default function ShopProductCard({ product }) {
-  // const { name, cover, price, colors, status, priceSale } = product;
-  // const { pet_passport_id, owner_id, metadata: { title, _description, media } } = product;
-
-  const data = {
-    "pet_passport_id": "2ed23244b32ae571885d8a98da9fbc8b23b64392109298275504cc68857a0868-Cyber Dog",
-    "metadata": {
-      "title": "Cyber Dog",
-      "description": "{\"species\": \"Dog\", \"breed\": \"Sheperd\", \"life-stage\": \"adult\"}",
-      "media": "https://ipfs.io/ipfs/Qmf3uctAUYBg56fcLxDr3Updda5F76bWaW1t7hfPfWtgz5"
-    },
-    "pet_owner_id": "2ed23244b32ae571885d8a98da9fbc8b23b64392109298275504cc68857a0868"
-  };
-
   const data2 ={
     "pet_passport_id": "2ed23244b32ae571885d8a98da9fbc8b23b64392109298275504cc68857a0868-Gigi9998",
     "metadata": {
@@ -60,6 +95,11 @@ export default function ShopProductCard({ product }) {
   const { title, description, media } = metadata;
   const { species, gender, breed, "life-stage": lifeStage } = JSON.parse(description);
 
+  const [flipped, setFlipped] = useState(false);
+
+  const handleFlip = () => {
+    setFlipped(!flipped);
+  };
 
 
   return (
@@ -90,9 +130,9 @@ export default function ShopProductCard({ product }) {
       <Typography variant="body2" sx={{ color: 'text.secondary', mb: 1 }}>
         {breed}
       </Typography>
-
+  
       <Divider sx={{ borderStyle: 'dashed' }} />
-
+  
       <Box display="grid" gridTemplateColumns="repeat(3, 1fr)" sx={{ py: 3 }}>
         <div>
           <Typography variant="caption" component="div" sx={{ mb: 0.75, color: 'text.disabled' }}>
@@ -100,15 +140,15 @@ export default function ShopProductCard({ product }) {
           </Typography>
           <Typography variant="caption">{gender}</Typography>
         </div>
-
+  
         <div>
           <Typography variant="caption" component="div" sx={{ mb: 0.75, color: 'text.disabled' }}>
             Life Stage
           </Typography>
-
+  
           <Typography variant="caption">{lifeStage}</Typography>
         </div>
-
+  
         <div>
           <Typography variant="caption" component="div" sx={{ mb: 0.75, color: 'text.disabled' }}>
             Rewards 
@@ -118,7 +158,18 @@ export default function ShopProductCard({ product }) {
           </Typography>
         </div>
       </Box>
-
+  
+      {/* Front Card 
+      <Box className={`${styles.flipCard} ${flipped ? styles.flipCardBack : styles.flipCardFront}`} onClick={handleFlip}>
+        <Typography variant="subtitle2">Front Card</Typography>
+      </Box> */}
+  
+      {/* Back Card 
+      <Box className={`${styles.flipCard} ${flipped ? styles.flipCardFront : styles.flipCardBack}`} onClick={handleFlip}>
+        <Typography variant="subtitle2">Back Card</Typography>
+      </Box> */}
+  
     </StyledCard>
   );
-}
+  
+}  
